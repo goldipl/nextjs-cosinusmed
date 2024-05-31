2e2b809af55369fe30e4a9ff9df26c0e9cc36171
goldipl <mgodlewski92@gmail.com>
2024-05-31 11:33:34 +0200

    fill hero template

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -7,33 +7,46 @@ import searchIcon from "./../../../public/icons/home/magnifying-glass-solid-whit
 const Hero = () => {
   return (
     <section className="home-hero">
-      <img className="cm-big-logo" src={heroCmIcon.src} alt="CM" height={600} />
-      <div className="homer-hero-text">
-        <h2>Piekno jest doskonałością z defektami</h2>
-        <span>Gilbert Adair</span>
-      </div>
-      <div className="home-hero-b-nav">
-        <div className="home-hero-b-nav__socials">
-          <span>Odwiedź nasze profile</span>
-          <a href="">
-            <img src={igIcon.src} alt="instagram" width={33} height={33} />
-          </a>
-          <a href="">
-            <img src={fbIcon.src} alt="facebook" width={16} height={30} />
-          </a>
+      <div className="home-hero-wrapper">
+        <img
+          className="cm-big-logo"
+          src={heroCmIcon.src}
+          alt="CM"
+          height={600}
+        />
+        <div className="home-hero-text">
+          <h2>
+            <span>Piekno jest</span>
+            <span>doskonałością</span>
+            <span>z defektami</span>
+          </h2>
+          <span className="author">Gilbert Adair</span>
         </div>
-        <div className="home-hero-b-nav__searchbar">
-          <form id="searchbar">
-            <input
-              type="search"
-              id="search"
-              name="search"
-              placeholder="wyszukaj w serwisie"
-            />
-            <button className="searchbar-btn">
-              <img src={searchIcon.src} alt="Szukaj" width={27} height={27} />
-            </button>
-          </form>
+      </div>
+      <div className="hero-bottom-bar">
+        <div className="hero-bottom-bar-wrapper">
+          <div className="hero-bottom-bar-wrapper__socials">
+            <span>Odwiedź nasze profile</span>
+            <a className="social-link" href="">
+              <img src={igIcon.src} alt="instagram" width={33} height={33} />
+            </a>
+            <a className="social-link" href="">
+              <img src={fbIcon.src} alt="facebook" width={16} height={30} />
+            </a>
+          </div>
+          <div className="hero-bottom-bar-wrapper__searchbar">
+            <form id="searchbar">
+              <input
+                type="search"
+                id="search"
+                name="search"
+                placeholder="wyszukaj w serwisie"
+              />
+              <button className="searchbar-btn">
+                <img src={searchIcon.src} alt="Szukaj" width={27} height={27} />
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
